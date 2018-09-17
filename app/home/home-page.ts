736853912de5756d37db9330f1ffdb3a70ac9c6b
NickIliev <nikolay.iliev@telerik.com>
2018-09-17 11:47:24 +0300

    feat: add example for passing/receiving context with shownModally and closeModal + update npm versions

--- app/home/home-page.ts
+++ app/home/home-page.ts
@@ -1,15 +1,23 @@
 import { View, EventData } from "tns-core-modules/ui/core/view";
 import { Frame } from "tns-core-modules/ui/frame";
+import {  ShownModallyData } from "tns-core-modules/ui/page";
 
 export function onModalFrame(args: EventData) {
     const view = args.object as View;
     const page = view.page;
 
     const frame = new Frame();
-    frame.navigate("modal/modal-page");
+    
+    frame.on("shownModally", (args: ShownModallyData) => {
+        frame.navigate({ 
+            moduleName: "modal/modal-page", 
+            context: args.context // passing the context from showModal
+        });
+    })
 
-    page.showModal(frame,
-        "context",
-        () => console.log("home-page modal frame closed"),
-        true);
+    page.showModal(frame,"Context passed from home-page",
+        (context) => {
+            console.log("home-page modal frame closed");
+            console.log("[home-page] Received context from closeModal callback: ", context);
+        }, true);
 }
